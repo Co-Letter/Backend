9f601cbdcd6f75b9989d5700a1e761d7a35fa180
donghui <louisp0215@gmail.com>
2023-12-14 02:25:26 +0900

    feat : 편지엔티티생성

---
+++ src/main/java/com/example/coletter/model/entity/Letter.java
@@ -0,0 +1,40 @@
+package com.example.coletter.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.bytecode.enhance.spi.EnhancementInfo;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Letter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "letter_id")
+    private long id;
+
+    @Column(name = "letter_content")
+    private String content;
+
+    @Column(name = "letter_writer")
+    private String writer;
+
+    @Column(name = "letter_secret")
+    private boolean secret;
+
+    private Timestamp letterCreateAt;
+
+    private int letterBackground;
+
+    private boolean letterReport;
+
+
+}
+
---
+++ src/main/java/com/example/coletter/repository/LetterRepository.java
@@ -0,0 +1,7 @@
+package com.example.coletter.repository;
+
+import com.example.coletter.model.entity.Letter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LetterRepository extends JpaRepository<Letter,Long> {
+}
