ffad35604e5e38d36aa0d347d03ae4304aee3b2e
SIWON990327 <pos06058@naver.com>
2023-12-19 22:01:24 +0900

    로그아웃 로직 추가

--- src/main/java/com/example/coletter/oauth/AuthController.java
+++ src/main/java/com/example/coletter/oauth/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.coletter.oauth;
 
 import com.example.coletter.kakao.KakaoLoginParams;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +12,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
-    private final OAuthLoginService oAuthLoginService;
 
-    @PostMapping("/kakao")
+    private final OAuthLoginService oAuthLoginService;
+    private final OAuthLogoutService oAuthLogoutService;
+
+    @Operation(summary = "카카오 로그인 및 회원가입")
+    @PostMapping("/kakaoLogin")
     public ResponseEntity<AuthTokens> loginKakao(@RequestBody KakaoLoginParams params) {
         System.out.println(params);
         log.info("{}",params);
         return ResponseEntity.ok(oAuthLoginService.login(params));
     }
+
+    @Operation(summary = "로그아웃")
+    @PostMapping("/kakaoLogout")
+    public String logout(@RequestHeader("Authorization") String authorization) {
+        return oAuthLogoutService.logout(authorization);
+    }
+
+
 
 }
