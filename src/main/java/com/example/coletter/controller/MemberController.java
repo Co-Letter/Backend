c353b200b3c28fb3feb6667fbfa3b97a3bdd7a6e
SIWON990327 <pos06058@naver.com>
2023-12-26 13:44:53 +0900

    편지함 전체 조회(페이징 미적용) 추가

--- src/main/java/com/example/coletter/controller/MemberController.java
+++ src/main/java/com/example/coletter/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/api/member")
 public class MemberController {
 
     private final MemberService memberService;
